15abcd5af7377e8fd604fb0ba9a7bb35f181d553
Mikep89 <michael.pineau89@gmail.com>
2020-10-24 21:42:32 -0400

    created new version in alembic and corrected typo within statuses

--- app/model.py
+++ app/model.py
@@ -24,7 +24,7 @@ class Statuses(Base):
     __tablename__="status"
     id = Column(Integer,primary_key=True)
     name = Column(String(50),nullable=False)
-    description = Column(String,250,nullable=False)
+    description = Column(String(250),nullable=False)
     projects = relationship('projects')
     tasks = relationship('tasks')
 
